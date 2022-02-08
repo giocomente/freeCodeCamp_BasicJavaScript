41bbd2a9654c5997347845766f4a15b822abc3c4
giocomente <aleshulmin@yandex.ru>
2022-02-08 14:49:34 +0400

    logical-order-in-if-else-statements

---
+++ logical-order-in-if-else-statements.js
@@ -0,0 +1,16 @@
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  }
+  
+  if (val < 10) {
+    return "Less than 10";
+  } 
+
+  
+   
+  return "Greater than or equal to 10";
+  
+}
+
+orderMyLogic(7);
